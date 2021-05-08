e4809c93b7c1f86003f756a590e0025070cc9442
Ricardo Bavaresco <ricardo.bav17@gmail.com>
2021-05-07 23:38:12 -0300

    servidor rodando lan

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -29,6 +29,7 @@ namespace AspNetServer.Controllers
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
+                idunico = index,
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)]
